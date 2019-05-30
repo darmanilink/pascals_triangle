32fc177f033f4643a4991e1706aa916628bbf5f9
darmanilink <44600791+darmanilink@users.noreply.github.com>
2019-05-30 14:56:25 -0700

    Update pascal.py

--- pascal.py
+++ pascal.py
@@ -1,4 +1,7 @@
+from math import log10
+from math import floor
 def pascals_triangle(tiers):
+  tiers += 1
   rows = list()
   for i in range(tiers):
     this_row = list()
@@ -22,18 +25,27 @@ def pascals_triangle(tiers):
 
 #TODO: find the length of the longest number and compensate whitespasce everywhere else accordingly
 def triangulate(list2d): #get it
+  ldigit = 0
+  for i in list2d:
+    for r in i:
+      tldigit = floor(log10(abs(r))) + 1
+      if tldigit > ldigit:
+        ldigit = tldigit
   formatted = ""
   length = len(list2d)-1
   for i in reversed(range(len(list2d))):
     index = length-i
     for j in range(i):
-      formatted += ' '
+      for l in range(ldigit):
+        formatted += ' '
     for k in list2d[index]:
-      formatted += (str(k) + ' ')
+      formatted += str(k)
+      for l in range(ldigit):#-(floor(log10(abs(k))) + 1)):
+        formatted += ' '
     formatted += '\n'  
   return formatted
 
-r = triangulate(pascals_triangle(15))
+r = triangulate(pascals_triangle(5))
 print (r)
 
       
